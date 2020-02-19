1ac588dd0e563216bb5050f9cb8c9dad438347f7
Thierry Leclaire <thierry_leclaire@videotron.ca>
2020-02-19 13:53:48 -0500

    Removed main text

--- JeuConnectFourConsole/JeuConnectFourConsole.cpp
+++ JeuConnectFourConsole/JeuConnectFourConsole.cpp
@@ -3,20 +3,5 @@
 using namespace std;
 int main()
 {
-	Board board = Board();
-	for (int i = 0; i < 3; i++)
-	{
-		board.changeO(0);
-	}
-	board.changeX(0);
-	for (int i = 0; i < 2; i++)
-	{
-		board.changeO(1);
-	}
-	board.changeX(1);
-	board.changeO(2);
-	board.changeX(2);
-	board.changeX(3);
-	board.print(cout);
-	cout << board.winner();
+	
 }
